72edc496cd00ef0e9214eaf280a955fc7381641f
gsaid6912 <gsaid6912@gmail.com>
2025-05-27 22:27:14 -0600

    test

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
       <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{
-          headerShown: true,  
+          headerShown: true, 
         }}
       >
         <Stack.Screen name="Login" component={LoginScreen} />
